c7f9d4e21e8797447b0c637a4014d758eff3c71d
Nathan Vanhoudnos <nathanvan@gmail.com>
2015-04-30 16:24:25 -0500

    first commit

---
+++ R/pstan.R
@@ -0,0 +1,144 @@
+#' Fit a model using Stan on multiple cores
+#'
+#' A wrapper for \code{rstan::stan} that, when possible, runs each MCMC chain on a
+#' separate core. See \code{help(stan)} for details of the syntax. In the documentation below,
+#' the stan help page is quoted without further attribution.
+#'
+#' \code{pstan} does all of the work of fitting a Stan model and returning the results as
+#' an instance of \code{stanfit}. First, it translates the Stan model to C++ code.
+#' Second, the C++ code is compiled into a binary shared object, which is loaded into
+#' the current R session (an object of S4 class \code{stanmodel} is created).
+#' Third, \code{pstan} calls \code{parallel::makeCluster} to setup a parallel worker
+#' process for every chain (or the maximum number of cores, whichever is less), distributes
+#' the compiled \code{stanmodel} to each worker, and uses
+#' \code{parallel:parLapply} to execute \code{stan:rstan} on each core.
+#' Finally, the chains are collected and wrapped into an object of S4 class \code{stanfit},
+#' which provides functions such as print, summary, and plot to inspect and retrieve the
+#' results of the fitted model.
+#'
+#' Note that \code{pstan} can also be used to sample again from a fitted model under
+#' different settings (e.g., different \code{iter}) by providing argument fit. In this case,
+#' the compiled C++ code for the model is reused.
+#'
+#' @param model_code A character string either containing the model definition or the name of a character string object in the workspace. This parameter is used only if parameter \code{file} is not specified. When \code{fit} is specified, the model compiled previously is used so specifying \code{model_code} is ignored.
+#' @param data An object of class \code{list, environment} providing the data for the model, or a vector of character strings for all the names of objects used as data in the working space. See \code{help(stan)} for further details.
+#' @param model_name A character string naming the model; defaults to \code{"anon_model"}. However, the model name would be derived from \code{file} or \code{model_code} (if \code{model_code} is the name of a character string object) if \code{model_name} is not specified.
+#' @param chains A positive integer specifying number of chains; defaults to 4. When possible, each chain is executed on its own core.
+#' @param fit An instance of S4 class \code{stanfit} derived from a previous fit; defaults to \code{NA}. If fit is not \code{NA}, the compiled model associated with the fitted result is re-used; thus the time that would otherwise be spent recompiling the C++ code for the model can be saved.
+#' @param seed The seed, a positive integer, for random number generation of Stan. The default is generated from 1 to the maximum integer supported by R so fixing the seed of R's random number generator can essentially fix the seed of Stan. When multiple chains are used, only one seed is needed, with other chains' seeds being generated from the first chain's seed to prevent dependency among the random number streams for the chains. When a seed is specified by a number, as.integer will be applied to it. If as.integer produces NA, the seed is generated randomly. We can also specify a seed using a character string of digits, such as "12345", which is converted to integer.
+#' @param pdebug If \code{TRUE}, \code{pstan} will create a file in the current working directory,
+#' \code{stan-debug-*}, that contains the output of the \code{rstan::stan} calls. Defaults to \code{TRUE}.
+#' @param ... The additional arguments are passed directly to \code{rstan::stan}. See \code{help(stan)} for documentation.
+#' @return Fitted results as an object of S4 class \code{stanfit}. If error occurs before or during sampling, and if \code{test_grad = TRUE}, the returned object would not contain samples. But the compiled binary object for the model is still included, so we can reuse the returned object for another sampling.
+#' @import rstan
+#' @import parallel
+#' @export
+pstan <- function(model_code, data, model_name = 'anon_model',
+                  chains = 4, fit = NULL, seed = NULL, pdebug = TRUE, ...) {
+
+  tmp.filename <- paste('stan-debug',
+                        gsub(' ', "-", Sys.time()), "txt",
+                        sep='.')
+  tmp.filename <- gsub(':', '.', tmp.filename )
+  
+  if (pdebug) message(paste('*** Parallel STAN run ***'))
+  if (pdebug) message(paste('Working directory:', getwd()))
+  if (pdebug) message('\n')
+    
+  ## Should we compile the model?
+  if( is.null(fit) ) {
+    if (pdebug) message(" + Compiling the STAN model.")
+    tryCatch( { 
+      fit <- stan( model_code = model_code,  model_name=model_name,
+                                chains=0, ... )},
+      error = function(e) {
+        message('model_code did not compile.')
+        stop(e)
+      }
+    )
+  } else {
+    if (pdebug) message(" + Compiled STAN model supplied.")
+  }
+
+  ## Did we supply a seed?
+  if ( is.null(seed) ) {
+    rng_seed <- sample.int(.Machine$integer.max, 1)
+  } else {
+    rng_seed <- seed
+  }
+
+  if (chains > 0 ) {
+    
+    num.chains <- chains
+    num.proc   <- min(detectCores(), num.chains)
+    if (pdebug) message(paste(" + Attempting ", num.chains," chains on", num.proc, "cores."))
+  
+    if (pdebug) message('   ... Creating the cluster.')
+    
+    tryCatch( {
+  
+      ## Define a filename for output, if necessary
+      cl <- NA
+      if( pdebug ) { 
+        message(paste('   ... Log file:', tmp.filename), "") 
+        cl <- makeCluster( min(num.proc, num.chains), outfile=tmp.filename)
+      } else {
+        cl <- makeCluster( min(num.proc, num.chains))
+      }
+      
+      if (pdebug) message('   ... Loading rstan on all workers.')
+      test.loading <- parLapply(cl, 1:length(cl), function(xx){require(rstan)})
+      if ( prod(unlist(test.loading)) != 1 | length(test.loading) != num.proc ) {
+        stop('Error: rstan did not load on all workers.')
+      }
+  
+      if (pdebug) message('   ... Exporting the fitted model and data to all workers.')
+      clusterExport(cl, c('fit', 'data', 'rng_seed'), envir=environment())
+  
+      if (pdebug) message('   ... Running parallel chains.')
+      fit.list <- parLapply(cl, 1:num.chains, function(ii) {
+        stan(fit      = fit,
+             data     = data,
+             seed     = rng_seed,
+             chains   = 1,
+             chain_id = ii,    ...)
+      })
+      
+    }, error = function(e) {
+      ## If the parallel execution failed, we still return the compiled 
+      ## fit object 
+      stopCluster(cl)
+      warning( e )
+      return( fit )
+    }, 
+      finally = {
+        stopCluster(cl)
+    })
+    
+    ## Combine the objects into a single stan object (and update fit)
+    if (num.chains > 1) {
+      tryCatch( {
+        fit <- sflist2stanfit(fit.list) }, 
+        error = function(e) {
+          message('\n')
+          message('Error in combining the results of parallel workers:')
+          message(paste('   ',e$message))
+          message('Returning a list with elements:')
+          message('   fit            the compiled fit object')
+          message('   parallel.list  the raw parallel results')
+          message('   error          the error object')
+          fit <<- list( fit = fit, 
+                        parallel.list = fit.list,
+                        error = e)
+        })
+    } else {
+      fit <- fit.list[[1]]
+    }
+    if (pdebug & 'stanfit' %in% is(fit)) message('   ... Finished!')
+    return( fit )
+    
+  } else {
+    ## Only wished to compile. 
+    return(fit)
+  }
+}
---
+++ tests/testthat/test-1-pstan.R
@@ -0,0 +1,154 @@
+## Tests for pstan.R
+
+## Weird errors unless I add the following:
+Sys.setenv("R_TESTS" = "")
+## from https://github.com/hadley/testthat/issues/144
+
+context('pstan.R tests')
+
+context(' + Checking error handling')
+  test_that("bad Stan code",{
+
+    schools_code <- 'data {
+        int<lower=0> J; // number of schools 
+        real y[J]; // estimated treatment effects
+        real<lower=0> sigma[J]; // s.e. of effect estimates 
+      }
+      parameters {
+        real mu; 
+        real<lower=0> tau;
+        real eta[J];
+      }
+      transformed parameters {
+        real theta[J];
+        for (j in 1:J)
+          theta[j] <- mu + tau * eta[j];
+      }
+      model {
+        eta ~ normal(0, 1);
+        y ~ normal(theta, sigma);
+      }'
+    
+    
+    ## There is no such type as realBUG. 
+    schools_code_buggy <- 'data {
+      int<lower=0> J; // number of schools 
+      real y[J]; // estimated treatment effects
+      real<lower=0> sigma[J]; // s.e. of effect estimates 
+    }
+      parameters {
+      realBUG mu; 
+      real<lower=0> tau;
+      real eta[J];
+    }
+      transformed parameters {
+      real theta[J];
+      for (j in 1:J)
+      theta[j] <- mu + tau * eta[j];
+    }
+      model {
+      eta ~ normal(0, 1);
+      y ~ normal(theta, sigma);
+    }'
+        
+      schools_dat <- list(J = 8, 
+                          y = c(28,  8, -3,  7, -1,  1, 18, 12),
+                          sigma = c(15, 10, 16, 11,  9, 11, 10, 18))
+      schools_dat_buggy <- list(J = 8, 
+                        y = c(28,  8, -3,  7, -1,  1, 18, 12),
+                        sigmaBug = c(15, 10, 16, 11,  9, 11, 10, 18))    
+      expect_that( 
+        pstan(model_code = schools_code_buggy,
+                     iter = 1000, chains = 4, seed=1, pdebug=FALSE),
+        throws_error())
+
+      valid.stanfit <- pstan( model_code = schools_code,
+                                chains=0, pdebug=FALSE)
+      broken.fit <- pstan(fit = valid.stanfit, data = schools_dat_buggy, 
+                          iter = 1000, chains = 4, seed=1, pdebug=FALSE)
+
+      expect_that( 
+        all.equal(broken.fit$fit, valid.stanfit), is_true() )
+      expect_that(
+        length( broken.fit$parallel.list ), equals(4) )
+            
+      })
+
+
+context(' + Checking equivalence of serial and parallel runs')
+    test_that("pstan",{
+        
+      # From https://github.com/stan-dev/rstan/wiki/RStan-Getting-Started
+      
+      schools_code <- 'data {
+        int<lower=0> J; // number of schools 
+        real y[J]; // estimated treatment effects
+        real<lower=0> sigma[J]; // s.e. of effect estimates 
+      }
+      parameters {
+        real mu; 
+        real<lower=0> tau;
+        real eta[J];
+      }
+      transformed parameters {
+        real theta[J];
+        for (j in 1:J)
+          theta[j] <- mu + tau * eta[j];
+      }
+      model {
+        eta ~ normal(0, 1);
+        y ~ normal(theta, sigma);
+      }'
+      
+      schools_dat <- list(J = 8, 
+                          y = c(28,  8, -3,  7, -1,  1, 18, 12),
+                          sigma = c(15, 10, 16, 11,  9, 11, 10, 18))
+      
+      ## Check if pstan and stan end up with the same random seed 
+      ## given R's current state
+      set.seed(1)
+      fit   <- stan(model_code = schools_code, data = schools_dat, 
+                    iter = 1000, chains = 4)      
+      set.seed(1)
+      fit.p.1 <- pstan(model_code = schools_code, data = schools_dat, 
+                      iter = 1000, chains = 4, pdebug=FALSE)
+      
+      ## Are the stan arguments the same? (Including seed.)
+      expect_that( all.equal(fit@stan_args, fit.p.1@stan_args), is_true() )
+      
+      ## Are the parameters with the same seed the same? 
+      expect_that(
+        extract( fit, pars='mu', permuted=FALSE, inc_warmup=TRUE  ),
+        is_identical_to( extract(fit.p.1, pars='mu', permuted=FALSE, inc_warmup=TRUE)))
+      expect_that(
+        extract( fit, pars='tau', permuted=FALSE, inc_warmup=TRUE  ),
+        is_identical_to( extract(fit.p.1, pars='tau', permuted=FALSE, inc_warmup=TRUE)))
+      expect_that(
+        extract( fit, pars='theta', permuted=FALSE, inc_warmup=TRUE  ),
+        is_identical_to( extract(fit.p.1, pars='theta', permuted=FALSE, inc_warmup=TRUE)))
+
+      ## Check that a different seed is generated even if the a fit object
+      ## is passed in 
+      fit.p.2 <- pstan(fit = fit, data = schools_dat, 
+                       iter = 1000, chains = 4, pdebug=FALSE)
+      expect_that( fit@stan_args[[1]]$seed == fit.p.2@stan_args[[1]]$seed, 
+                   is_false()) 
+      
+      ## Are parameters with a different seed different? 
+      expect_that(
+        ( all.equal( 
+            extract( fit, pars='mu', permuted=FALSE, inc_warmup=TRUE  ),
+            extract( fit.p.2, pars='mu', permuted=FALSE, inc_warmup=TRUE )) == TRUE),
+        is_false()) 
+      expect_that(
+        ( all.equal( 
+          extract( fit, pars='tau', permuted=FALSE, inc_warmup=TRUE  ),
+          extract( fit.p.2, pars='tau', permuted=FALSE, inc_warmup=TRUE )) == TRUE),
+        is_false()) 
+      expect_that(
+        ( all.equal( 
+          extract( fit, pars='theta', permuted=FALSE, inc_warmup=TRUE  ),
+          extract( fit.p.2, pars='theta', permuted=FALSE, inc_warmup=TRUE )) == TRUE),
+        is_false())   
+            
+    })
